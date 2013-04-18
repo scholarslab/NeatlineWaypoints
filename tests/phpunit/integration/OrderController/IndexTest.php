0f7ea9a32262e41176b6d8540e82dd747812129c
David McClure <david.mcclure@virginia.edu>
2013-04-18 18:49:09 -0400

    Tests for record order saving.

---
+++ tests/phpunit/integration/OrderController/IndexTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 cc=76; */
+
+/**
+ * Tests for INDEX action in order controller.
+ *
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+class OrderControllerTest_Index extends NeatlineItemTray_TestCase
+{
+
+
+    /**
+     * INDEX should update record weights.
+     */
+    public function testBaseMarkup()
+    {
+
+        $exhibit = $this->__exhibit();
+        $record1 = $this->__record($exhibit);
+        $record2 = $this->__record($exhibit);
+        $record3 = $this->__record($exhibit);
+
+        $this->request->setMethod('POST')->setPost(array(
+            'order' => array(
+                $record1->id,
+                $record2->id,
+                $record3->id
+            )
+        ));
+
+        $this->dispatch('neatline-item-tray');
+        $record1 = $this->reload($record1);
+        $record2 = $this->reload($record2);
+        $record3 = $this->reload($record3);
+
+        $this->assertEquals($record1->weight, 0);
+        $this->assertEquals($record2->weight, 1);
+        $this->assertEquals($record3->weight, 2);
+
+    }
+
+
+}
