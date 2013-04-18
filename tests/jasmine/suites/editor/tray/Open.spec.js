107f697307d0a9ed6b88a2107dc35b3d90a6a075
David McClure <david.mcclure@virginia.edu>
2013-04-18 19:33:12 -0400

    Sketches in editor Jasmine tests.

---
+++ tests/jasmine/suites/editor/tray/Open.spec.js
@@ -0,0 +1,22 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+describe('Open', function() {
+
+
+  beforeEach(function() {
+    _tray.loadEditor();
+  });
+
+
+  it('should load records');
+
+
+});
--- tests/jasmine/suites/editor/tray/Save.spec.js
+++ tests/jasmine/suites/editor/tray/Save.spec.js
@@ -2,15 +2,13 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
 
 /**
- * .
- *
  * @package     omeka
  * @subpackage  neatline
  * @copyright   2012 Rector and Board of Visitors, University of Virginia
  * @license     http://www.apache.org/licenses/LICENSE-2.0.html
  */
 
-describe('Editor Stub', function() {
+describe('Save', function() {
 
 
   beforeEach(function() {
@@ -18,9 +16,10 @@ describe('Editor Stub', function() {
   });
 
 
-  it('test', function() {
-    expect(true).toBeTruthy();
-  });
+  it('should issue POST request when "Save" is clicked');
+  it('should flash notification when save succeeds');
+  it('should flash notification when save fails');
+  it('should refresh the exhibit when save succeeds');
 
 
 });
