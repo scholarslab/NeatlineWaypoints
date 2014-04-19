f34158169956988a650421e662e295b47fca91be
David McClure <davidwilliammcclure@gmail.com>
2014-04-19 11:05:07 -0700

    Fixes .gitignore error that was leaving /neatine tests untracked. :/

---
+++ tests/phpunit/tests/fixtures/neatline/EventsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 cc=80; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-Waypoints
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+class FixturesTest_NeatlineEvents extends NeatlineWaypoints_Case_Fixture
+{
+
+
+    public function testRecords()
+    {
+
+        $record1 = $this->_record($this->exhibit);
+        $record2 = $this->_record($this->exhibit);
+        $record3 = $this->_record($this->exhibit);
+
+        $record1->title = 'title1';
+        $record2->title = 'title2';
+        $record3->title = 'title3';
+
+        $record1->save();
+        $record2->save();
+        $record3->save();
+
+        $this->_writeRecordsApiFixture($this->exhibit,
+            'NeatlineEvents.json'
+        );
+
+    }
+
+
+}
---
+++ tests/phpunit/tests/fixtures/neatline/ListTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 cc=80; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-Waypoints
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+class FixturesTest_NeatlineList extends NeatlineWaypoints_Case_Fixture
+{
+
+
+    public function testRecordLoading()
+    {
+
+        $record1 = $this->_record($this->exhibit);
+        $record2 = $this->_record($this->exhibit);
+        $record3 = $this->_record($this->exhibit);
+
+        $record1->title = 'title1';
+        $record2->title = 'title2';
+        $record3->title = 'title3';
+        $record1->weight = 1;
+        $record2->weight = 2;
+        $record3->weight = 3;
+
+        $record1->save();
+        $record2->save();
+        $record3->save();
+
+        $this->request->setQuery(array('order' => 'weight'));
+
+        $this->_writeRecordsApiFixture($this->exhibit,
+            'NeatlineList.regular.json'
+        );
+
+        $record1->weight = 3;
+        $record2->weight = 2;
+        $record3->weight = 1;
+
+        $record1->save();
+        $record2->save();
+        $record3->save();
+
+        $this->request->setQuery(array('order' => 'weight'));
+
+        $this->_writeRecordsApiFixture($this->exhibit,
+            'NeatlineList.changed.json'
+        );
+
+    }
+
+
+    public function testEmpty()
+    {
+        $this->_writeRecordsApiFixture($this->exhibit,
+            'NeatlineList.empty.json'
+        );
+    }
+
+
+}
